819a03ca00a34a76be2b7e6b069dec739327f35b
Stefano Longobardi <stefano.longobardi.8@gmail.com>
2024-07-06 15:13:18 +0100

    Improved code + added data chunking

--- model.py
+++ model.py
@@ -12,29 +12,37 @@ from sklearn import preprocessing
 from sklearn.metrics import confusion_matrix
 
 
-def main():
-    loadpath = Path(os.getcwd()) / "data"
+def plot_all_columns(df, timepoints, savepath=None):
+    # dimensions for a 16:9 slide
+    width_in_inches = 13.33
+    height_in_inches = 7.5
+    # plotting
+    n_cols = len(df.columns.values)
+    fig, axes = plt.subplots(n_cols, 1, figsize=(width_in_inches, height_in_inches))
+    for i, (col, axis) in enumerate(zip(df.columns.values, axes.flat)):
+        axis.plot(df.index.values, df[col])
+        axis.set_ylabel(col)
+        if i == n_cols - 1:
+            axis.set_xlabel("Date")
+        for ti in timepoints:
+            axis.axvline(ti, ls="--", c="r")
+    fig.tight_layout()
+    if savepath is None:
+        plt.show()
+    else:
+        plt.savefig(savepath / "figure.png", dpi=600)
+        plt.close()
 
-    df = pd.read_csv(loadpath / "T1.csv")
-    print(df.columns.values)
 
-    # Define the time format
+def fix_time_column(df):
     time_format = "%d %m %Y %H:%M"
-
-    # Convert the "Date/Time" column to string and then apply datetime conversion
     df["Date/Time"] = (
         df["Date/Time"].astype(str).apply(lambda x: datetime.strptime(x, time_format))
     )
-    # Print the modified DataFrame
     df.set_index("Date/Time", inplace=True)
 
-    fig, axes = plt.subplots(len(df.columns.values), 1)
-    for col, axis in zip(df.columns.values, axes.flat):
-        axis.plot(df.index.values, df[col])
-        axis.set_ylabel(col)
-    fig.tight_layout()
-    plt.show()
 
+def compute_turbine_status(df, threshold=10):
     # Defining Power deficit values in percentage
     power_deficit_percentage = (
         100
@@ -43,12 +51,10 @@ def main():
     )
     df.insert(4, "power_deficit_percentage", power_deficit_percentage)
 
-    # considering 0 for zero values of theoritcal power
+    # considering 0 for zero values of theoretical power
     df.loc[df["Theoretical_Power_Curve (KWh)"] == 0, "power_deficit_percentage"] = 0
 
     # Defining the status label of the turbines wih two numeric values: 1 for "ok"; 0 for "not ok"
-    threshold = 10
-
     condition = (df["power_deficit_percentage"] < threshold) & (
         df["power_deficit_percentage"] > -threshold
     )
@@ -62,25 +68,49 @@ def main():
         0  # meaning that the turbine does work near theoretical power curve
     )
 
-    print(df.head())
 
-    # fig, axis = plt.subplots(1, 1)
-    # axis.plot(df.index.values, df["turbine_status"])
-    # plt.show()
+def main():
+    abspath = Path(os.getcwd())
 
-    x = df.drop(columns=["turbine_status"])
-    y = df["turbine_status"]
+    loadpath = abspath / "data"
+    df = pd.read_csv(loadpath / "T1.csv")
 
-    x_train, x_test, y_train, y_test = train_test_split(
-        x, y, test_size=0.2, random_state=0
-    )
+    fix_time_column(df)
 
-    regressor = LogisticRegression()
-    regressor.fit(x_train, y_train)
+    # timeframe min-max
+    start_time = df.index.min()
+    end_time = df.index.max()
+    # generate time intervals
+    time_ranges = pd.date_range(start=start_time, end=end_time, periods=5)
+    # slice the DataFrame into 4 parts based on these intervals
+    df1 = df[(df.index >= time_ranges[0]) & (df.index < time_ranges[1])]
+    df2 = df[(df.index >= time_ranges[1]) & (df.index < time_ranges[2])]
+    df3 = df[(df.index >= time_ranges[2]) & (df.index < time_ranges[3])]
+    df4 = df[(df.index >= time_ranges[3]) & (df.index <= time_ranges[4])]
 
-    result = regressor.score(x_test, y_test)
+    timepoints = [dfi.index.values[0] for dfi in [df1, df2, df3, df4]]
+    savepath = abspath / "figures"
+    savepath.mkdir(exist_ok=True)
+    plot_all_columns(df, timepoints, savepath=savepath)
 
-    print(result)
+    #
+    compute_turbine_status(df1)
+
+    # plot_all_columns(df1)
+
+    # x = df.drop(columns=["turbine_status"])
+    # y = df["turbine_status"]
+
+    # x_train, x_test, y_train, y_test = train_test_split(
+    #     x, y, test_size=0.2, random_state=0
+    # )
+
+    # regressor = LogisticRegression()
+    # regressor.fit(x_train, y_train)
+
+    # result = regressor.score(x_test, y_test)
+
+    # print(result)
 
 
 if __name__ == "__main__":
